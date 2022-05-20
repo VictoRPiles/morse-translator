650cff7a3de2b7289a4cd9bf5b576e64a20f5caf
VictoRPiles <victorpilesclase@gmail.com>
2022-05-20 13:55:54 +0200

    Función main simple para probar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,15 @@
-/** @return All args concatenated in one String */
-fun getMessageFromArgs(args: Array<String>): String {
-	var message = ""
-	for (word in args) {
-		message += "$word "
-	}
-	return message
-}
-
-/** @return Not null String with the message. */
-fun input(): String {
-	print("Type your message -> ")
-	return readLine() ?: return input()
-}
-
-fun checkFlags(args: Array<String>) {
-	if (args[0].startsWith("-")) {
-		// TODO: 20/05/2022  numero del arg? recursivo ???
-	}
-}
-
 fun main(args: Array<String>) {
-	val message: String = if (args.isNotEmpty()) {
-		checkFlags(args)
-		getMessageFromArgs(args)
-	} else input()
-	println(message)
-	// TODO: 20/05/2022 Añadir flag al comando, para saber si args es morse o alfabeto -e (--encode), -d (--decode)
-	// TODO: 20/05/2022 Menu encode o decode
+	val input = Input(args)
+	val encoder = MorseEncoder()
+	val decoder = MorseDecoder()
+
+	val message = if (args.isNotEmpty()) input.getMessageFromArgs() else input.cliInput()
+
+	val newMessage = when (input.checkFlags()) {
+		"encode" -> "Encoded message -> " + encoder.encode(message)
+		"decode" -> "Decoded message -> " + decoder.decode(message)
+		else -> "Unknown option flag"
+	}
+
+	println(newMessage)
 }
