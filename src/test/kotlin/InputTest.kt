65c9a5ab74bcba8e4365550e979a0de14c6ea764
VictoRPiles <victorpilesclase@gmail.com>
2022-05-24 12:58:01 +0200

    Tests para la clase Input

---
+++ src/test/kotlin/InputTest.kt
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+internal class InputTest {
+
+	@Test
+	fun getMessageFromArgs() {
+		var args: Array<String>
+		var input: Input
+
+		/* Encode flag */
+		args = arrayOf("-e", "hola", "morse   ")
+		input = Input(args)
+		assertEquals("hola morse", input.getMessageFromArgs())
+
+		/* Decode flag */
+		args = arrayOf("-d", ".- .", ".- --   ")
+		input = Input(args)
+		assertEquals(".- . .- --", input.getMessageFromArgs())
+
+		/* Wrong flag */
+		args = arrayOf("-z", "abc", "def")
+		input = Input(args)
+		assertEquals("-z abc def", input.getMessageFromArgs())
+	}
+
+	@Test
+	fun checkFlags() {
+		var args: Array<String>
+		var input: Input
+
+		/* Encode flag */
+		args = arrayOf("-e", "hola", "morse")
+		input = Input(args)
+		assertEquals("encode", input.checkFlags())
+
+		/* Decode flag */
+		args = arrayOf("-d", ".- .", ".- --   ")
+		input = Input(args)
+		assertEquals("decode", input.checkFlags())
+
+		/* Wrong flag */
+		args = arrayOf("-z", "abc", "def")
+		input = Input(args)
+		assertNull(input.checkFlags())
+	}
+
+	@Test
+	fun isMorseMessage() {
+		val input = Input(arrayOf("args"))
+		assertTrue(input.isMorseMessage(".- .. .- ---"))
+		assertTrue(input.isMorseMessage(".- @ .. .- ---"))
+		assertTrue(input.isMorseMessage(".- @ .. .- ------"))
+		assertFalse(input.isMorseMessage(".- a .. .- ---"))
+		assertFalse(input.isMorseMessage(".- 1 .. .- ---"))
+	}
+}
