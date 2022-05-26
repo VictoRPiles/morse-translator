000f8a7e4c62a839d53f89eaf22dd3a64c56e329
VictoRPiles <victorpilesclase@gmail.com>
2022-05-26 09:21:19 +0200

    Tests para la clase MorseDecoder

---
+++ src/test/kotlin/MorseDecoderTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+internal class MorseDecoderTest {
+
+	@Test
+	fun decode() {
+		val morseDecoder = MorseDecoder()
+		assertEquals("ea", morseDecoder.decode(". .-"))
+		assertEquals("ea", morseDecoder.decode(".   .-"))
+		assertEquals("e a", morseDecoder.decode(". / .-"))
+		assertEquals("e a", morseDecoder.decode("./.-"))
+		try {
+			morseDecoder.decode("----------")
+			fail()
+		} catch (_: IllegalArgumentException) {
+		}
+		try {
+			morseDecoder.decode(".- ......")
+			fail()
+		} catch (_: IllegalArgumentException) {
+		}
+		try {
+			morseDecoder.decode(".- a")
+			fail()
+		} catch (_: IllegalArgumentException) {
+		}
+	}
+}
